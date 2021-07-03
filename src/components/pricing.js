f45235b5e272a5641f3930fa18effa6dfe48fbfd
hundehausen <mail@grischadaum.de>
2021-07-03 18:50:24 +0200

    price calculation

--- src/components/pricing.js
+++ src/components/pricing.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { Slider } from "antd"
 
 const sliderProps = {
@@ -18,27 +18,45 @@ const handleListenerSliderTooltip = (value) => {
   }
 }
 
-const Pricing = () => (
-  <div className="pricing-container">
-    <h2>Preise</h2>
-    <div className="pricing-sliders">
-      <Slider
-        defaultValue={sliderProps.defaultListeners}
-        min={sliderProps.minListeners}
-        max={sliderProps.maxListeners}
-        tooltipVisible
-        tipFormatter={(value) => `${value} Teilnehmer`}
-      ></Slider>
-      <Slider
-        defaultValue={sliderProps.defaultHours}
-        min={sliderProps.minHours}
-        max={sliderProps.maxHours}
-        step={0.5}
-        tooltipVisible
-        tipFormatter={handleListenerSliderTooltip}
-      ></Slider>
+const calculatePrice = (listeners, hours) => {
+    return hours * 300 + listeners * 20;
+}
+
+const Pricing = () => {
+  const [listeners, setListeners] = useState(sliderProps.defaultListeners)
+  const [hours, setHours] = useState(sliderProps.defaultHours)
+  const [price, setPrice] = useState(0)
+
+  useEffect(()=> {
+      const price = calculatePrice(listeners, hours);
+      setPrice(price);
+  }, [listeners, hours])
+
+  return (
+    <div className="pricing-container">
+      <h2>Preise</h2>
+      <div className="pricing-sliders">
+        <Slider
+          defaultValue={sliderProps.defaultListeners}
+          min={sliderProps.minListeners}
+          max={sliderProps.maxListeners}
+          tooltipVisible
+          tipFormatter={(value) => `${value} Teilnehmer`}
+          onChange={setListeners}
+        ></Slider>
+        <Slider
+          defaultValue={sliderProps.defaultHours}
+          min={sliderProps.minHours}
+          max={sliderProps.maxHours}
+          step={0.5}
+          tooltipVisible
+          tipFormatter={handleListenerSliderTooltip}
+          onChange={setHours}
+        ></Slider>
+      </div>
+      <span className="price">{`Preis: ${price} €`}</span>
     </div>
-  </div>
-)
+  )
+}
 
 export default Pricing
