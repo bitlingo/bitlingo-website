bc87477ce2a37c2fa8e5aad2fc77633049f102c8
hundehausen <mail@grischadaum.de>
2021-07-03 18:33:00 +0200

    slider props

--- src/components/pricing.js
+++ src/components/pricing.js
@@ -1,17 +1,44 @@
 import React from "react"
 import { Slider } from "antd"
 
-const prices = {
-    defaultListeners: 10,
-    defaultHours: 1
+const sliderProps = {
+  defaultListeners: 10,
+  defaultHours: 1,
+  minListeners: 1,
+  minHours: 0.5,
+  maxListeners: 100,
+  maxHours: 8,
+}
+
+const handleListenerSliderTooltip = (value) => {
+  if (value === 1) {
+    return `${value} Stunde`
+  } else {
+    return `${value} Stunden`
+  }
 }
 
 const Pricing = () => (
-    <div className="pricing">
-        <h2>Preise</h2>
-        <Slider defaultValue={prices.defaultListeners} tooltipVisible></Slider>
-        <Slider defaultValue={prices.defaultHours} tooltipVisible></Slider>
+  <div className="pricing-container">
+    <h2>Preise</h2>
+    <div className="pricing-sliders">
+      <Slider
+        defaultValue={sliderProps.defaultListeners}
+        min={sliderProps.minListeners}
+        max={sliderProps.maxListeners}
+        tooltipVisible
+        tipFormatter={(value) => `${value} Teilnehmer`}
+      ></Slider>
+      <Slider
+        defaultValue={sliderProps.defaultHours}
+        min={sliderProps.minHours}
+        max={sliderProps.maxHours}
+        step={0.5}
+        tooltipVisible
+        tipFormatter={handleListenerSliderTooltip}
+      ></Slider>
     </div>
+  </div>
 )
 
 export default Pricing
