b47260badd93a1edfef1f95f08140994dae45650
hundehausen <mail@grischadaum.de>
2021-07-04 15:12:07 +0200

    link to lectures

--- src/components/header.js
+++ src/components/header.js
@@ -1,32 +1,34 @@
-import {Link} from "gatsby"
+import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 import logo from "../../static/images/logo.png"
 
-const Header = ({siteTitle}) => (
-    <header>
-        <div className={"container"}>
-            <div className={"top-menu"}>
-                <div className={"logo"}>
-                    <Link to="/" title={"Bitlingo"}>
-                        <img alt={"Logo"} src={logo} width="200"/>
-                    </Link>
-                </div>
-
-                <div className={"get-started"}>
-                    <a href={"mailto:info@bitlingo.de"}>Anfragen</a>
-                </div>
-            </div>
+const Header = ({ siteTitle }) => (
+  <header>
+    <div className={"container"}>
+      <div className={"top-menu"}>
+        <div className={"logo"}>
+          <Link to="/" title={"Bitlingo"}>
+            <img alt={"Logo"} src={logo} width="200" />
+          </Link>
         </div>
-    </header>
+
+        <div className={"get-started"}>
+          <Link to="/lectures" title={"Bitlingo Vorträge"}>
+            Zu unseren Vorträgen
+          </Link>
+        </div>
+      </div>
+    </div>
+  </header>
 )
 
 Header.propTypes = {
-    siteTitle: PropTypes.string,
+  siteTitle: PropTypes.string,
 }
 
 Header.defaultProps = {
-    siteTitle: ``,
+  siteTitle: ``,
 }
 
 export default Header
