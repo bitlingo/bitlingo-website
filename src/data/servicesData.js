c607317d1f00f0decda5e8cc64d2d414edde4ba8
Grischa Daum <grischa.daum@sevdesk.de>
2022-07-07 21:17:28 +0200

    update services

--- src/data/servicesData.js
+++ src/data/servicesData.js
@@ -2,18 +2,92 @@ import labyrinth from "../../static/images/services/labyrinth.webp";
 import lostPassword from "../../static/images/services/lostPassword.jpg";
 
 const servicesData = [
-    {
-        title: `ETH an ETC oder umgekehrt gesendet. Was tun?`,
-        text: `Biltingo unterstützt dich dabei deine ETH / ETC wiederherzustellen`,
-        price: `30 % des Betrags`,
-        picture: labyrinth,
-    },
-    {
-        title: `Ich habe das Passwort meiner Wallet vergessen! Hilfe!`,
-        text: `Biltingo unterstützt dich dabei dein Passwort wiederherzustellen. Voraussetzung... du hast deine Wallet gespeichert.`,
-        price: `30 % des Betrags`,
-        picture: lostPassword,
-    },
+  {
+    title: `ETH an ETC oder umgekehrt gesendet. Was tun?`,
+    text: `Bitlingo unterstützt dich dabei deine ETH / ETC wiederherzustellen`,
+    price: `30 % des Betrags`,
+    picture: labyrinth,
+  },
+  {
+    title: `Ich habe das Passwort meiner Wallet vergessen! Hilfe!`,
+    text: `Bitlingo unterstützt dich dabei dein Passwort wiederherzustellen. Voraussetzung... du hast deine Wallet gespeichert.`,
+    price: `30 % des Betrags`,
+    picture: lostPassword,
+  },
+  {
+    title: `Kryptowährungen kaufen, verkaufen, versenden`,
+    text: `Wir helfen dir dabei deine Kryptowährungen zu kaufen, verkaufen und versenden. Dabei sind wir nicht mit einer bestimmten Exchange verpartnert. Wir finden gemeinsam die passende Handelsplattform für deine Bedürfnisse.`,
+    price: `69 € / Stunde`,
+    picture: lostPassword,
+  },
+  {
+    title: `Krypto-Sparplan erstellen`,
+    text: `Bitlingo unterstützt dich dabei deinen Krypto-Sparplan zu erstellen. Dabei kannst du deine Kryptowährungen auf einem eigenen Sparplan verwalten.`,
+    price: `69 € / Stunde`,
+    picture: lostPassword,
+  },
+  ,
+  {
+    title: `Coins von einer Exchange auf die eigene (Hardware) Wallet verschicken`,
+    text: `Immer wieder merken wir, dass gerade Einsteiger Probleme mit der Wallet-Software haben. Daher bieten wir dir die Möglichkeit, deine Coins auf deine Hardware-Wallet zu verschicken. Dies ist eine sehr schnelle und einfache Möglichkeit, deine Coins sicher zu verwahren, denn auf einer Exchange sollte man seine Coins generell nicht liegen lassen.`,
+    price: `39 € / Session`,
+    picture: lostPassword,
+  },
+  ,
+  {
+    title: `Hardware Wallet einrichten (Ledger, BitBox, Trezor)`,
+    text: `Wo du deine Hardware-Wallet am besten kaufen kannst und wie diesen anschließend sicher eingerichtet wird, dabei kann dir Bitlingo helfen.`,
+    price: `99 € / Einrichtung`,
+    picture: lostPassword,
+  },
+  ,
+  {
+    title: `Paper Wallet, Mobile Wallet erstellen`,
+    text: `Nicht immer ist eine Hardware-Wallet eine gute Wahl. Daher bieten wir dir die Möglichkeit, eine Paper-Wallet oder eine Mobile-Wallet zu erstellen.`,
+    price: `39 € / Wallet`,
+    picture: lostPassword,
+  },
+  ,
+  {
+    title: `Browser Wallet einrichten (MetaMask, MyEtherWallet)`,
+    text: `Gerade wenn man das erste Mal im Ethereum Ökosystem handeln möchte, um beispielsweise ein NFT zu ewerben, benötigt man einen sicheren Browser mit einer vertrauenswürdigen Browser Wallet. Hierbei haben schon viele Menschen Geld an Betrüger verloren. Bitlingo weiß genau, worauf es ankommt.`,
+    price: `39 € / Wallet`,
+    picture: lostPassword,
+  },
+  ,
+  {
+    title: `NFTs kaufen, erstellen und verkaufen`,
+    text: `Wie du ein NFT kaufen, erstellen und verkaufen möchtest, kann Bitlingo dir helfen.`,
+    price: `10 % des Betrags`,
+    picture: lostPassword,
+  },
+  ,
+  {
+    title: `Staking, Lending, Liquidity Mining von Kryptowährungen`,
+    text: `Du hast bestimmt schon mal von Staking oder Lending gehört. Du weißt aber nicht genau wie das funktioniert und wie du teilnehmen kannst? Kein Problem. Bitlingo unterstütz dich dabei.`,
+    price: `69 € / Stunde`,
+    picture: lostPassword,
+  },
+  ,
+  {
+    title: `Kryptowährungen als Zahlungsmittel im eigenen Geschäft akzeptieren`,
+    text: `Was gibt es zu beachten bei der Annahme für Kryptowöhrungen als Zahlungsmittel im eigenen Geschäft? Wie das am unkomplizierten, sichersten und wie das mit den Steuern ist, kann Bitlingo dir helfen.`,
+    price: `99 € / Stunde`,
+    picture: lostPassword,
+  },
+  ,
+  {
+    title: `Kryptowährungen vererben`,
+    text: `Ein Thema, über das man nicht so oft denkt, ist was bei einem Todesfall mit dem Nachlass der Kryptowährungen passieren soll. Welche Möglichkeiten für dich in Frage kommen, das klären wir bei einem Gespräch.`,
+    price: `79 € / Stunde`,
+    picture: lostPassword,
+  },
+  {
+    title: `Steuertool für Kryptowährungen verwenden`,
+    text: `Wie du ein Steuertool für Kryptowährungen verwenden kannst, kann Bitlingo dir helfen.`,
+    price: `79 € / Stunde`,
+    picture: lostPassword,
+  },
 ];
 
 export default servicesData;
